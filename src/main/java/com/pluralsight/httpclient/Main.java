0a814e96bcc7c895b8e8eda34d1970b9d58e6f82
Jeff Wong <v-jwong1@expedia.com>
2020-03-26 19:43:28 +1000

    create HttpRequest instance using builder

--- src/main/java/com/pluralsight/httpclient/Main.java
+++ src/main/java/com/pluralsight/httpclient/Main.java
@@ -1,10 +1,16 @@
 package com.pluralsight.httpclient;
 
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
 
 public class Main {
     public static void main(String[] args) {
         final HttpClient httpClient = HttpClient.newHttpClient();
-        System.out.println(httpClient);
+        final HttpRequest httpRequest = HttpRequest
+                .newBuilder(URI.create("https://pluralsight.com"))
+                .build();
+
+        httpClient.send(request);
     }
 }
