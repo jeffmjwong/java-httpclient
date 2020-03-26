03619fb644ec140f19ea1790167671e04739d3d6
Jeff Wong <v-jwong1@expedia.com>
2020-03-26 19:46:23 +1000

    get httpResponse after sending a request

--- src/main/java/com/pluralsight/httpclient/Main.java
+++ src/main/java/com/pluralsight/httpclient/Main.java
@@ -3,14 +3,22 @@ package com.pluralsight.httpclient;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class Main {
     public static void main(String[] args) {
         final HttpClient httpClient = HttpClient.newHttpClient();
-        final HttpRequest httpRequest = HttpRequest
+
+        final HttpRequest request = HttpRequest
                 .newBuilder(URI.create("https://pluralsight.com"))
                 .build();
 
-        httpClient.send(request);
+        try {
+            final HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+            System.out.println(response.headers());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
