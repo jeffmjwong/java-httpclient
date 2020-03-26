8af5e0bfcb220852483649a7a9cd5913e7024bd1
Jeff Wong <v-jwong1@expedia.com>
2020-03-26 19:38:33 +1000

    create http client

--- src/main/java/com/pluralsight/httpclient/Main.java
+++ src/main/java/com/pluralsight/httpclient/Main.java
@@ -1,7 +1,10 @@
 package com.pluralsight.httpclient;
 
+import java.net.http.HttpClient;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Http Client!");
+        final HttpClient httpClient = HttpClient.newBuilder().build();
+        System.out.println(httpClient);
     }
 }
