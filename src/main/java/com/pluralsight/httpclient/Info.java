1f673336c1c82ce7af6e539259f5c8e2a44ba118
Jeff Wong <v-jwong1@expedia.com>
2020-03-27 15:12:16 +1000

    complete Info class

---
+++ src/main/java/com/pluralsight/httpclient/Info.java
@@ -0,0 +1,21 @@
+package com.pluralsight.httpclient;
+
+public class Info {
+    private String seed;
+    private int results;
+    private int page;
+    private String version;
+
+    public String getSeed() {
+        return seed;
+    }
+    public int getResults() {
+        return results;
+    }
+    public int getPage() {
+        return page;
+    }
+    public String getVersion() {
+        return version;
+    }
+}
