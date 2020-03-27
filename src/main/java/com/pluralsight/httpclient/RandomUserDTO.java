7fc6629da41d80b83e49ec7fc7d85554005e00b8
Jeff Wong <v-jwong1@expedia.com>
2020-03-27 15:27:31 +1000

    create UserDTO class

--- src/main/java/com/pluralsight/httpclient/RandomUserDTO.java
+++ src/main/java/com/pluralsight/httpclient/RandomUserDTO.java
@@ -1,8 +1,14 @@
 package com.pluralsight.httpclient;
 
+import java.util.ArrayList;
+
 public class RandomUserDTO {
+    private ArrayList<UserDTO> results;
     private InfoDTO info;
 
+    public ArrayList<UserDTO> getResults() {
+        return results;
+    }
     public InfoDTO getInfo() {
         return info;
     }
