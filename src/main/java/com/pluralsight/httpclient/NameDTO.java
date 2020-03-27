e3daedf235285e7915b500716b010fe8d396748b
Jeff Wong <v-jwong1@expedia.com>
2020-03-27 16:14:23 +1000

    use @ToString annotation from lombok for NameDTO class

--- src/main/java/com/pluralsight/httpclient/NameDTO.java
+++ src/main/java/com/pluralsight/httpclient/NameDTO.java
@@ -1,15 +1,12 @@
 package com.pluralsight.httpclient;
 
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 public class NameDTO {
     private String title;
     private String first;
     private String last;
-
-    @Override
-    public String toString() {
-        return "Name{title='" + title + "', first='" + first + "', last='" + last + "'}";
-    }
 }
