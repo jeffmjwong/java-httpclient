bdbfa3bbbac927f8d2338d4bb54b9c565d637cdc
Jeff Wong <v-jwong1@expedia.com>
2020-03-27 16:09:48 +1000

    use lombok @Getter annotation for InfoDTO class

--- src/main/java/com/pluralsight/httpclient/InfoDTO.java
+++ src/main/java/com/pluralsight/httpclient/InfoDTO.java
@@ -1,23 +1,13 @@
 package com.pluralsight.httpclient;
 
+import lombok.Getter;
+
+@Getter
 public class InfoDTO {
     private String seed;
     private int results;
     private int page;
     private String version;
-
-    public String getSeed() {
-        return seed;
-    }
-    public int getResults() {
-        return results;
-    }
-    public int getPage() {
-        return page;
-    }
-    public String getVersion() {
-        return version;
-    }
 
     @Override
     public String toString() {
