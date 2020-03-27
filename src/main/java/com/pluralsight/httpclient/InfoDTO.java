be5914f42593dbb54f891e027c95ba86bc4ef3b7
Jeff Wong <v-jwong1@expedia.com>
2020-03-27 16:13:19 +1000

    use @ToString annotation from lombok for InfoDTO class

--- src/main/java/com/pluralsight/httpclient/InfoDTO.java
+++ src/main/java/com/pluralsight/httpclient/InfoDTO.java
@@ -1,16 +1,13 @@
 package com.pluralsight.httpclient;
 
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 public class InfoDTO {
     private String seed;
     private int results;
     private int page;
     private String version;
-
-    @Override
-    public String toString() {
-        return "Info{seed='" + seed + "', results=" + results + ", page=" + page + ", version='" + version + "'}";
-    }
 }
