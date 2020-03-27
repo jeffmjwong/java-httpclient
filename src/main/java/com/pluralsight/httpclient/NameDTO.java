dded84e586943fc80818f6201f02d7ad544ae176
Jeff Wong <v-jwong1@expedia.com>
2020-03-27 15:42:20 +1000

    create toString method for NameDTO class

--- src/main/java/com/pluralsight/httpclient/NameDTO.java
+++ src/main/java/com/pluralsight/httpclient/NameDTO.java
@@ -1,4 +1,12 @@
 package com.pluralsight.httpclient;
 
 public class NameDTO {
+    private String title;
+    private String first;
+    private String last;
+
+    @Override
+    public String toString() {
+        return "Name{title='" + title + "', first='" + first + "', last='" + last + "'}";
+    }
 }
